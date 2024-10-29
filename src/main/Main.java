2ae7cfcad6cc17c98144e0428ae30a4df70ca13c
Shepard Summers <115957978+shepardsummers@users.noreply.github.com>
2024-10-29 17:49:16 -0400

    Added c funct

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,14 +4,21 @@ public class Main {
 	
 	// INPUT VARIABLES
 	
-	// system constants
+	// assorted constants
 	public final static int m = 398600; // km^3/s^2
 	public final static int eRad = 6378; // km
+	public final static int alt = 500; // km
+	public final static int tLat = 60; // deg
+	public final static int tAlt = eRad + alt; // km
 	
 	// time (s)
 	public final static int t1 = 0;
 	public final static int t2 = 5 * 60;
 	public final static int t3 = 10 * 60;
+	
+	public final static double tau = t3 - t1;
+	public final static double tau1 = t1 - t2;
+	public final static double tau3 = t3 - t2;
 	
 	// mean anomaly (degrees)
 	public final static double theta1 = 150.000;
@@ -28,11 +35,26 @@ public class Main {
 	public final static double dec2 = 27.2993;
 	public final static double dec3 = 29.8982;
 	
+	// test
+	public final static int r2 = 900; //km
+	
 	public static void main(String[] args) {
 		
-		System.out.println(m);
+		double[] c = findC();
 		
+		System.out.println(c[0]);
+		System.out.println(c[1]);
 	}
+	
+	static double[] findC() {
+		
+		double c1 = (tau3/tau) * (1 + (m/Math.pow(r2, 3)) * (Math.pow(tau, 2) - Math.pow(tau3, 2)));
+		double c3 = (-tau1/tau) * (1 + (m/Math.pow(r2, 3)) * (Math.pow(tau, 2) - Math.pow(tau1, 2)));
+		
+		double[] out = {c1,c3};
+		return out;
+	}
+	
 	
 	
 }
